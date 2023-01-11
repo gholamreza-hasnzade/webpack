f5f6358635590e19cec0d442abed925908c6b854
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-11 15:34:24 +0330

    add jquey

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,22 +1,14 @@
-import $ from "query";
 import webpackImage from "./webpack_image.png";
 
 class Image {
     render() {
-        /*   const $ = document;
+        const $ = document;
         const img = $.createElement("img");
         img.alt = "webpack iamge";
         img.src = webpackImage;
         img.width = 450;
         const body = $.querySelector("body");
-        body.appendChild(img); */
-        const img = $(`<img />`, {
-            alt: "webpack iamge",
-            src: webpackImage,
-            width: 450
-        });
-        const body = $("body");
-        body.append(img)
+        body.appendChild(img);
     }
 }
 export default Image;
